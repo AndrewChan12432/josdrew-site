b54038e8440469147a8ce22a5113fa9527077c4d
AndrewChan12432 <chanjiunlik@yahoo.com>
2022-03-13 15:50:04 +0800

    update css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="make-center">
+        <div className="make-flex">
+          <h1 className='moving-around'>Under</h1>
+          <h1>Construction</h1>
+        </div>
+        <h6>We are upgrading our website and will be on-line very soon</h6>
+        <p>Reach out to us through Email,Facebook or Whatsapp (preferable)</p>
+        <div className='full-width'>
+          <input type="text" placeholder="example@email.com"></input>
+          <input type="submit" />
+          <div className='make-side'>
+            <a href="https://www.facebook.com/JoshDrewWebStudio" id="fb-icon">
+              <i class="fa-brands fa-facebook-square fa-3x"></i>
+            </a>
+            <a href="https://wa.me/60179906278?text=I'm%20interested%20for%20your%20services" id="whatsapp-icon">
+              <i class="fa-brands fa-whatsapp-square fa-3x"></i>
+          </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
